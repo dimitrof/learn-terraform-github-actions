de603728748ea2e4533e1af99ac3ba6738ca0d78
Nick Dimitroff <ndimitroff@gmail.com>
2023-07-19 18:15:07 -0400

    updated main

--- main.tf
+++ main.tf
@@ -30,7 +30,8 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-09e67e426f25ce0d7"
+  # ami                    = "ami-09e67e426f25ce0d7"
+  ami                    = "ami-06ca3ca175f37dd66"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
